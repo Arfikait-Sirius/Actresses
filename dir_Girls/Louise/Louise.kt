247c4465f41772d1abf36e4dd88b477a65fd97d8
Arfikait Sirius <Arfikait.Sirius@gmail.com>
2021-03-10 22:23:16 +0900

    Learn Emily, Louise. - Emily#fnDispStr(), Louise#fnLowerAll(), Louise#fnUpperFirst().

--- dir_Girls/Louise/Louise.kt
+++ dir_Girls/Louise/Louise.kt
@@ -4,4 +4,14 @@ object Louise {
      fun fnUpperAll( base: String ): String {
           return base.toUpperCase()
      }
+
+     fun fnLowerAll( base: String ): String {
+          return base.toLowerCase()
+     }
+
+     fun fnUpperFirst( base: String ): String {
+          val baseLower = base.toLowerCase().takeLast( base.length - 1 )
+          val baseUpper = base.toUpperCase().take( 1 )
+          return "$baseUpper$baseLower"
+     }
 }
