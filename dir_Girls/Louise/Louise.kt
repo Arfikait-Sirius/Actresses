e369a0c3b970f27d950559ed5365442799e8373a
Arfikait Sirius <Arfikait.Sirius@gmail.com>
2021-03-10 22:43:36 +0900

    Retake Louise. - Make up.

--- dir_Girls/Louise/Louise.kt
+++ dir_Girls/Louise/Louise.kt
@@ -10,8 +10,8 @@ object Louise {
      }
 
      fun fnUpperFirst( base: String ): String {
-          val baseLower = base.toLowerCase().takeLast( base.length - 1 )
           val baseUpper = base.toUpperCase().take( 1 )
+          val baseLower = base.toLowerCase().takeLast( base.length - 1 )
           return "$baseUpper$baseLower"
      }
 }
