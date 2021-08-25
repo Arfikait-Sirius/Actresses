4c0806b554f9ac9d6eaa4a3a0b927dbb986efafb
Arfikait Sirius <arfikait.sirius@gmail.com>
2021-08-25 21:31:35 +0900

    Retake Louise. - Make up.

--- dir_Girls/Louise/Louise.kt
+++ dir_Girls/Louise/Louise.kt
@@ -2,16 +2,16 @@ package actresses.girls
 
 object Louise {
      fun fnUpperAll( base: String ): String {
-          return base.toUpperCase()
+          return base.uppercase()
      }
 
      fun fnLowerAll( base: String ): String {
-          return base.toLowerCase()
+          return base.lowercase()
      }
 
      fun fnUpperFirst( base: String ): String {
-          val baseUpper = base.toUpperCase().take( 1 )
-          val baseLower = base.toLowerCase().takeLast( base.length - 1 )
+          val baseUpper = base.uppercase().take( 1 )
+          val baseLower = base.lowercase().takeLast( base.length - 1 )
           return "$baseUpper$baseLower"
      }
 }
